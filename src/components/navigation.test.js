08dc1e1b5f63c8eb16f629dbf66895b7f0df5ba5
Adrian L Thomas <adrian.thomas@gmail.com>
2021-02-20 18:36:06 +0000

    Revert "simplified"
    This reverts commit f4944692cf8037f231f6f284a9d3d418a957ef8c.

--- src/components/navigation.test.js
+++ src/components/navigation.test.js
@@ -1,21 +1,19 @@
-// const { GetCleanPath } = require('./navigation')
+const { GetCleanPath } = require('./navigation')
 
-// describe('GetCleanPath()', () => {
-//     test('should return empty string when root path provided', () => {
-//         const expected = "";
+describe('GetCleanPath()', () => {
+    test('should return empty string when root path provided', () => {
+        const expected = "";
 
-//         const actual = GetCleanPath("/");
+        const actual = GetCleanPath("/");
 
-//         expect(actual).toEqual(expected);
-//     });
+        expect(actual).toEqual(expected);
+    });
     
-//     test('should return last part of path when multiple levels provided', () => {
-//         const expected = "four";
+    test('should return last part of path when multiple levels provided', () => {
+        const expected = "four";
 
-//         const actual = GetCleanPath("/one/two/three/four");
+        const actual = GetCleanPath("/one/two/three/four");
 
-//         expect(actual).toEqual(expected);
-//     })
-// });
-
-// TODO - if url is same then css class should be selected
+        expect(actual).toEqual(expected);
+    })
+});
