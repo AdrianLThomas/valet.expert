e511f7edc9cac12e4fead5e794dbfd6a11101841
Adrian L Thomas <adrian.thomas@gmail.com>
2021-02-25 21:51:01 +0000

    move gql out of bg image and in to hero

--- src/components/background.js
+++ src/components/background.js
@@ -1,26 +1,9 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
 import css from './background.module.scss';
 
 import BackgroundImage from 'gatsby-background-image'
 
-const BackgroundSection = ({ className }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        desktop: file(relativePath: { eq: "Hero.jpg" }) {
-          childImageSharp {
-            fluid(quality: 75, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `
-  )
-
-  const imageData = data.desktop.childImageSharp.fluid
-
+const BackgroundSection = ({ className, imageData }) => {
   return (
     <BackgroundImage
       Tag="section"
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import StyledBackgroundSection from './background';
 import css from './hero.module.scss';
+import { graphql, useStaticQuery } from 'gatsby'
 import './layout.module.scss';
 
 export default function Hero({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "Hero.jpg" }) {
+          childImageSharp {
+            fluid(quality: 75, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
   return (
     <div className={css.container}>
-      <StyledBackgroundSection className={css.background}></StyledBackgroundSection>
+      <StyledBackgroundSection className={css.background} imageData={data.desktop.childImageSharp.fluid}></StyledBackgroundSection>
     </div>
   );
 }
