67da55bad2787e09d56cb6ee1f85e37b18423ded
Adrian L Thomas <adrian.thomas@gmail.com>
2021-03-12 23:20:54 +0000

    animation

--- src/components/footer.js
+++ src/components/footer.js
@@ -43,6 +43,11 @@ const useStyles = makeStyles((theme) => {
     },
     socialIcon: {
       padding: '10px 20px 10px 20px',
+      '&:hover': {
+          transform: 'translateY(3px)',
+      },
+      transition: 'transform 500ms',
+      willChange: 'transform',
     },
     socialContainer: {
       display: 'flex',
