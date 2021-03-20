0b2089facda9c1c2f3900c7b2b2a955f3bb170c5
Adrian L Thomas <adrian.thomas@gmail.com>
2021-03-20 21:07:09 +0000

    fix scss order problem

--- src/components/card-grid.js
+++ src/components/card-grid.js
@@ -9,9 +9,36 @@ import {
 } from '@material-ui/core';
 import { Link } from 'gatsby';
 
-import { root, cardHeader, button } from './card-grid.module.scss';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+  cardHeader: {
+    textAlign: 'center',
+  },
+  button: {
+    textAlign: 'center',
+    fontSize: '1em',
+    padding: '10px',
+    width: '100%',
+    fontWeight: 'bold',
+    transition: 'transform 500ms',
+    willChange: 'transform',
+    border: '1px solid black',
+    '&:hover': {
+      transform: 'translateY(3px)'
+    }
+  },
+});
 
 export default function CardGrid({ className, cards }) {
+  const { root, cardHeader, button } = useStyles();
+
   return (
     <Grid container spacing={1} alignItems="stretch" className={className}>
       {cards.map((card) => (
