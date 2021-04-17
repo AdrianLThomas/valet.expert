673361eebfa7c7ad43abb268838a1ca5aa4273ed
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2021-04-17 23:15:36 +0100

    more tests

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,38 @@
 describe('Basic Build Check', () => {
-  it('Check build and about me link', () => {
+  it('Home Page', () => {
     cy.visit(Cypress.env('endpoint'));
 
     cy.contains("Matty's Mobile Valeting & Detailing");
+  });
 
+  it('About Me', () => {
+    cy.visit(Cypress.env('endpoint'));
     cy.contains('About Me').click();
-
     cy.url().should('include', '/about-me/');
+    cy.contains('My business was established in 2004')
+  });
+
+  it('Services & Pricing', () => {
+    cy.visit(Cypress.env('endpoint'));
+
+    cy.contains('Services & Pricing').click();
+    cy.url().should('include', '/services-and-pricing/');
+    cy.contains('Mini Valet')
+  });
+
+  it('Areas Covered', () => {
+    cy.visit(Cypress.env('endpoint'));
+
+    cy.contains('Areas Covered').click();
+    cy.url().should('include', '/areas-covered/');
+    cy.contains('YO12')
+  });
+
+  it('Services & Pricing', () => {
+    cy.visit(Cypress.env('endpoint'));
+
+    cy.contains('Contact Me').click();
+    cy.url().should('include', '/contact-me/');
+    cy.contains('MattysMobileValeting@hotmail.com')
   });
 });
