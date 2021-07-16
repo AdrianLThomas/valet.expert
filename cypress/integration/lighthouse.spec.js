64e4c708cc6b2ac7c89a9112cd7c485f6494a221
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2021-07-16 21:26:03 +0100

    cypress retry

--- cypress/integration/lighthouse.spec.js
+++ cypress/integration/lighthouse.spec.js
@@ -2,11 +2,17 @@ beforeEach(() => {
   cy.visit('/');
 });
 
-// As I'm at the mercy of GHA cloud runners,
-// retries and an overall lower threshold
-// than reality is required (see: `cypress.json`).
-jest.retryTimes(5);
-
-it('should pass the audits', function () {
-  cy.lighthouse();
-});
+it(
+  'should pass the audits',
+  {
+    retries: {
+      runMode: 4,
+    },
+  },
+  function () {
+    // As I'm at the mercy of GHA cloud runners,
+    // retries and an overall lower threshold
+    // than reality is required (see: `cypress.json`).
+    cy.lighthouse();
+  }
+);
