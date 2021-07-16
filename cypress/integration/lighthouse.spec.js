5f5db3e0b27a32ff9f45976aa257c57e816e371d
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2021-07-16 21:15:23 +0100

    ignore no-undef for jest

--- cypress/integration/lighthouse.spec.js
+++ cypress/integration/lighthouse.spec.js
@@ -5,7 +5,7 @@ beforeEach(() => {
 // As I'm at the mercy of GHA cloud runners,
 // retries and an overall lower threshold
 // than reality is required (see: `cypress.json`).
-jest.retryTimes(5)
+jest.retryTimes(5);
 
 it('should pass the audits', function () {
   cy.lighthouse();
