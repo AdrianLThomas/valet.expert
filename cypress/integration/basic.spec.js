4a0b9bfb84daff3aa289d6acf65e174de38931c1
Adrian L Thomas <adrian.thomas@gmail.com>
2021-09-04 22:56:34 +0100

    sitemap test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -43,4 +43,8 @@ describe('Basic Build Check', () => {
   it('robots.txt', () => {
     cy.request(`/robots.txt`).its('body').should('include', 'Allow: /');
   });
+
+  it('sitemap.xml', () => {
+    cy.request(`/sitemap/sitemap-index.xml`).its('body').should('include', '<sitemap>');
+  });
 });
