fc51f6573066815cf1bd3ce758df0b5d6d66ddab
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2021-09-15 11:40:40 +0100

    terrible change

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -70,25 +70,7 @@ export default function AboutMe({ location }) {
       <Hero type="beading" classOverride={classes.hero}>
         <h2 className={classes.heroTitle}>{'About Me'}</h2>
       </Hero>
-      <p>
-        My business was established in 2004 in the vibrant seaside town of
-        Scarborough.
-      </p>
 
-      <p>
-        I have been in the trade for around {yearsInTrade} years, starting off
-        as a valeter for Arundale Peugeot, Scarborough. I then assisted the
-        running of a local valeting business, before seeing the opportunity to
-        work for myself as a mobile valeter. The business has gone from strength
-        to strength and continues it's success to this day.
-      </p>
-
-      <p>
-        It's now {now.getFullYear()} and I've been running my own business for
-        around {yearsRunning} years, operating within the area to provide a high
-        quality, professional valeting service for thousands of customers
-        throughout the region.
-      </p>
 
       <div className={classes.root}>
         <Grid container spacing={2}>
